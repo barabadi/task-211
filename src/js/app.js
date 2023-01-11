a8a6024baf6d9870f886b01a17189ad579cee0be
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-11 21:47:08 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,13 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
+const anchorEl = document.querySelector('a');
+
+
+anchorEl.href = "#boom";
+
+
+const boomEl = document.querySelector('.boom');
+
+
+boomEl.id = "boom";
